d893c4c173af41532a76356755bcdc9ffafb6ec2
Joel Viza <jviza.job@gmail.com>
2025-03-18 00:15:57 -0500

    Se agrega funcion sortearAmigo

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const inputAmigo = document.getElementById("amigo");
-const listaAmigos =[];
+let listaAmigos =[];
 const listaAmigosUl = document.getElementById("listaAmigos");
+const resultadoUl = document.getElementById("resultado");
 
 function agregarAmigo(){
+    //consdicion para que no dejen en blanco
     if (inputAmigo.value == ""){
         alert("Debe ingresar un nombre");
+        return;
     }
+    //se agrega un nombre
     listaAmigos.push(inputAmigo.value);
+
+    //aqui se esta actualizando el html con los nombres de los amigos ingresados
     listaAmigosUl.innerHTML += `<li>${inputAmigo.value}</li>` 
+}
+
+
+function sortearAmigo(){
+    //se busca un numero random para encontrar ala migo secreto
+    const sorteo = Math.floor(Math.random() * listaAmigos.length);
+
+    //con el numero encontrado se allo el amigo secreto
+    const amigoSecreto =listaAmigos[sorteo];
+
+    //aqui se esta mostrando en el html el nombres del amigos secreto
+    resultadoUl.innerHTML = `<li>El amigo secreto es: ${amigoSecreto} </li>`
 }
